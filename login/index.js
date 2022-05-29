e864031adc22f3ba055f98af53b6a6f44a10a59f
Júlio Faria <juliano988>
2022-05-29 20:36:28 -0300

    ?. subistituido por .

--- login/index.js
+++ login/index.js
@@ -8,14 +8,14 @@ const UserModel = require('../db/model.js').UserModel
 
 router.post('/', function (req, res) {
 
-  const userName = req.body?.userName
-  const password = req.body?.password
+  const userName = req.body.userName
+  const password = req.body.password
 
   UserModel.findOne({ userName: userName }, null, { lean: true }, function (err, data) {
     if (err) { return res.status(500).json({ message: 'Internal error.', err: err }) }
     if (!data) { return res.status(404).json({ message: 'User not found or wrong password!' }) }
 
-    bcrypt.compare(password, data?.password, function (err, result) {
+    bcrypt.compare(password, data.password, function (err, result) {
       if (err) { return res.status(500).json({ message: 'Internal error.', err: err }) }
       if (!result) { return res.status(404).json({ message: 'User not found or wrong password!' }) }
 
