985a4cd2c31387bb319d5f1d07d50aa6685cf7e9
Júlio Faria <juliano988>
2022-10-01 15:51:45 -0300

    RFG-2 parcialmente implementado

--- user/index.js
+++ user/index.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+const UserModel = require('../db/model.js').UserModel
 
-router.get('/', passport.authenticate('jwt', { session: false }),(req, res) => {
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
 
   delete req.user.password
   delete req.user.userName
 
   res.send(req.user)
+
+});
+
+router.put('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  const id = req.body._id;
+  const name = req.body.name;
+  const teamName = req.body.teamName;
+  const role = req.body.role;
+
+  UserModel.findByIdAndUpdate(id, { name: name, teamName: teamName, role: role }, function (err, data) {
+    if (err) { return res.status(500).json({ message: 'Internal error.', err: err }) }
+
+    res.status(204).send();
+
+  })
 
 });
 
