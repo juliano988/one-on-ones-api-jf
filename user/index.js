5abce2a34a706ac45c02a1cd707e86f7c24b1bb6
Júlio Faria <juliano988>
2022-11-13 15:55:56 -0300

    Rota de delete adicionada

--- user/index.js
+++ user/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 const UserModel = require('../db/model.js').UserModel
+const MeetingModel = require('../db/model.js').MeetingModel;
 
 router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
 
@@ -23,6 +24,24 @@ router.put('/', passport.authenticate('jwt', { session: false }), (req, res) => 
     if (err) { return res.status(500).json({ message: 'Internal error.', err: err }) }
 
     res.status(204).send();
+
+  })
+
+});
+
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  const id = req.body._id;
+
+  UserModel.findByIdAndDelete(id, function (err, data) {
+    if (err) { return res.status(500).json({ message: 'Internal error.', err: err }) }
+
+    MeetingModel.deleteMany({ _id: { $in: data._id_meeting } }, function (data, err) {
+      if (err) { return res.status(500).json({ message: 'Internal error.', err: err }) }
+
+      res.status(204).send();
+
+    })
 
   })
 
